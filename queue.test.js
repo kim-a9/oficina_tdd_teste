76a671cb5fe325298fe04cbbb475825f26340941
kim-a9 <kimberlysantosalves@gmail.com>
2025-05-11 00:39:30 -0300

    resolução desafio create queue

--- queue.test.js
+++ queue.test.js
@@ -8,18 +8,32 @@ describe('Queue', () => {
   })
 
   it('Deve ser capaz de calcular o tamanho da fila', () => {
-    // Seu código aqui
+    const queue = new Queue();
+    expect(queue.size()).toBe(0)
+
   })
 
   it('Deve ser capaz de adicionar um item na fila', () => {
-   // Seu código aqui
+    const queue = new Queue();
+    queue.add('item1');
+    expect(queue.size()).toBe(1);
   })
 
   it('Deve escolher o primeiro item da fila', () => {
-    // Seu código aqui
+    const queue = new Queue();
+    queue.add('item1');
+    expect(queue.peek()).toBe('item1');
+
   })
 
   it('Deve remover o primeiro item da fila', () => {
-    // Seu código aqui
+    const queue = new Queue();
+    queue.add('item1');
+    queue.add('item2');
+    queue.add('item3');
+    expect(queue.dequeue()).toBe('item1');
+    expect(queue.dequeue()).toBe('item2');
+    expect(queue.dequeue()).toBe('item3');
+    // expect(queue.dequeue()).toBeUndefined();
   })
 })
